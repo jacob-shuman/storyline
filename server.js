9c5caa53aaffa2dc7d19b021c5f2cef6c5b137a1
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2020-01-23 20:31:20 -0500

    Added server.js with simple routing

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+// const hostname = '10.102.112.181';
+const PORT = 10034;
+
+app.use(express.static(path.join(__dirname, "dist", "storyline")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "storyline", "index.html"));
+});
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("Working!");
+});
+
+require("http")
+  .createServer(app)
+  .listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
